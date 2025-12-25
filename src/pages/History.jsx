1a24949b1889c3e9c2a6fd3759ac7af4000c6434
ExceptionPK <saleunomas@gmail.com>
2025-12-25 13:46:54 +0100

    xd

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -483,17 +483,12 @@ const HistoryItem = ({
                                         value={editOriginalPrice}
                                         onChange={(e) => {
                                             let value = e.target.value;
-                                            value = value
-                                                .replace(/[^0-9,.]/g, '')
-                                                .replace(/\./g, ',');
+                                            value = value.replace(/[^0-9,]/g, '');
                                             const parts = value.split(',');
                                             if (parts.length > 2) {
                                                 value = parts[0] + ',' + parts.slice(1).join('');
                                             }
-                                            if (parts[1] && parts[1].length > 2) {
-                                                value = parts[0] + ',' + parts[1].slice(0, 2);
-                                            }
-                                            setEditPrice(value);
+                                            setEditOriginalPrice(value);
                                         }}
                                         onKeyDown={(e) => {
                                             if (e.key === '.') {
@@ -541,15 +536,10 @@ const HistoryItem = ({
                                         value={editPrice}
                                         onChange={(e) => {
                                             let value = e.target.value;
-                                            value = value
-                                                .replace(/[^0-9,.]/g, '')
-                                                .replace(/\./g, ',');
+                                            value = value.replace(/[^0-9,]/g, '');
                                             const parts = value.split(',');
                                             if (parts.length > 2) {
                                                 value = parts[0] + ',' + parts.slice(1).join('');
-                                            }
-                                            if (parts[1] && parts[1].length > 2) {
-                                                value = parts[0] + ',' + parts[1].slice(0, 2);
                                             }
                                             setEditPrice(value);
                                         }}
